30b94cc7f4fc2247b3656a4e2e564a1ba3ecc2d3
MClaraFerreira5 <mariaferreira578.9cl@gmail.com>
2025-04-01 20:56:40 -0300

    salvando

--- detectar_objetos.py
+++ detectar_objetos.py
@@ -5,21 +5,22 @@ model = YOLO("yolov8n.pt")
 
 cap = cv2.VideoCapture(0) 
 
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Fazer a detecção
+    
     results = model(frame)
 
-    # Exibir os resultados
+    
     for result in results:
         frame = result.plot()
 
     cv2.imshow("YOLO Detecção", frame)
 
-    # Pressione 'q' para sair
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
